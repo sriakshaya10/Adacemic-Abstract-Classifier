05202b04bc409182271ad09ca913b199612a480d
sri akshaya kodali <sriakshaya_kodali@srmap.edu.in>
2026-05-21 18:05:58 +0530

    Update email configuration with new sender details

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'seetharamakartheekchallapalli@gmail.com'  # Change this
+app.config['MAIL_USERNAME'] = 'akshaya.ska9@gmail.com'  # Change this
 app.config['MAIL_PASSWORD'] = 'tjkq hnxn lxcj kmsk'      # Change this
-app.config['MAIL_DEFAULT_SENDER'] = 'seetharamakartheekchallapalli@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'akshaya.ska9@gmail.com'
 
 db = SQLAlchemy(app)
 mail = Mail(app)
